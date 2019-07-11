27c0872c63c4b6eb4c4a6e2f72a3cb9442c2b7de
Priest <>
2019-07-11 13:39:03 +0300

    first commit

---
+++ src/app/summaries/summaries.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-summaries',
+  templateUrl: './summaries.component.html',
+  styleUrls: ['./summaries.component.css']
+})
+export class SummariesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
